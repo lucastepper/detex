148c1f324c9cebec00b1d6f899da5310b260673f
lucas tepper <lucas.tepper.91@gmail.com>
2024-04-24 08:41:42 +0200

    More Doc

--- detex.py
+++ detex.py
@@ -3,7 +3,13 @@ import sys
 from pathlib import Path
 
 
-def get_single_caption(text):
+def get_single_caption(text: str):
+    r"""Get the text for the next caption in the text. The caption is enclosed in \caption{}.
+    Args:
+        text: The text to search for the caption.
+    Returns:
+        The caption text
+    """
     idx_start = 0
     # find where caption starts
     caption_found = False
@@ -19,9 +25,6 @@ def get_single_caption(text):
     num_open_curly = 1
     idx_end = idx_start
     while idx_end < len(text):
-        char = text[idx_end]
-        context = text[idx_end - 10 : idx_end + 10]
-        context1 = text[idx_end : idx_end + 10]
         if text[idx_end] == "{":
             num_open_curly += 1
         elif text[idx_end] == "}":
@@ -32,16 +35,20 @@ def get_single_caption(text):
     return text[idx_start:idx_end]
 
 
-def block_text(text_to_block, line_len: int = 100):
-    """Convert text into blocked text with max line_len. This function does not really work, yet."""
+def block_text(text_to_block: str, line_len: int = 100):
+    """Convert text into blocked text with max line_len. This function does not really work, yet.
+    Args:
+        text_to_block: The text to convert to a blocked text
+        line_len: The maximum length of a line
+    Returns:
+        Blocked text
+    """
     lines = [line for line in text_to_block.split("\n") if line]
     # split words and remove empty strings
     lines = [[w for w in line.split(" ") if w] for line in lines]
     # remember which lines are short, as we want to preserve the line breaks there
     len_lines = [len(" ".join(line)) for line in lines]
 
-    # text_to_block = text_to_block.replace("\n", " ").replace("\t", " ")
-    # words = [w for w in text_to_block.split(" ") if w]
     text_blocked = ""
     word_count = 0
     while len(lines) > 0:
@@ -72,8 +79,6 @@ def block_text(text_to_block, line_len: int = 100):
             else:
                 text_blocked += "\n"
                 word_count = 0
-    # if not text_blocked.endswith("\n"):
-    #     text_blocked += "\n"
     return text_blocked
 
 
@@ -83,6 +88,12 @@ def get_text_enclosed(
     """Find the text between prefix and suffix and return it. If remove, replace
     prefix MATCH suffix with empty string. Throws an error if more than one hit found
     If block, blocks the hit.
+    Args:
+        text: The text to search in
+        prefix: The prefix enclosing the text to find
+        suffix: The suffix enclosing the text to find
+        remove: If True, remove the hit from the text
+        block: If True, call block_text on the hit
     """
     hit = re.findall(prefix + r"(.*)" + suffix, text, flags=re.DOTALL)
     if not hit:
@@ -97,27 +108,51 @@ def get_text_enclosed(
 
 
 def delete_tex_command(text: str, command: str):
-    """Delete all occurences of a given latex command in text"""
+    r"""Delete all occurences of a given latex command in text.
+    Args:
+        text: The text to delete the command from
+        command: The command to search for, if it does not start with \\, it is added
+            It needs to start with an escaped \, i.e. \\cite
+    Returns:
+        The text with the command removed
+    """
     if not r"\\" in command:
         command = r"\\" + command
     return re.sub(command + r"(?:(?:\[.*\])?\n?\{[^}]+\})?", "", text)
 
 
 def replace_tex_command_with_body(text: str, command: str):
-    """Find all occurences of a given latex command in text and replace it with the body; {$BODY}"""
+    r"""Find all occurences of a given latex command in text and replace it with the body; {$BODY}
+    Args:
+        text: The text to search in
+        command: The command to search for, if it does not start with \\, it is added
+            It needs to start with an escaped \, i.e. \cite
+    Returns:
+        The text with the command replaced with the body
+    """
     if not r"\\" in command:
         command = r"\\" + command
     return re.sub(command + r"{([^}]*)}", r"\1", text)
 
 
 def delte_comments(text: str):
-    """Delete all comments, by removing everything after % in a line"""
+    """Delete all comments, by removing everything after % in a line
+    Args:
+        text: The text to remove comments from
+    Returns:
+        The text with comments removed
+    """
     text = re.sub(r"%.*\n", "", text)
     return text
 
 
 def delete_equations(text: str):
-    """Delete all equations in equation, array or align environment"""
+    """Delete all equations in equation, array or align environment.
+    Args:
+        text: The text to remove equations from
+    Returns:
+        The text with equations removed
+    """
     equation_qualifiers = ["equation", "align", "array", "subequations"]
     for eqn_quali in equation_qualifiers:
         text = re.sub(
@@ -130,7 +165,12 @@ def delete_equations(text: str):
 
 
 def get_title(text) -> str:
-    """Search for title in document"""
+    """Search for title in document
+    Args:
+        text: The text to search for the title
+    Returns:
+        The title of the document
+    """
     hit = re.search(r"\\title(?:\[.*\])?\n?\{([^{]+)\}", text)
     if len(hit.groups()) >= 1:
         return hit.group(1)
@@ -138,7 +178,12 @@ def get_title(text) -> str:
 
 
 def get_document_body(text) -> str:
-    r"""Search for the body inside \begin{document} \end{document}"""
+    r"""Search for the body inside \begin{document} \end{document}
+    Args:
+        text: The text of the latex document
+    Returns:
+        The body of the document
+    """
     hit = re.search(r"\\begin{document}(.*)\\end{document}", text, flags=re.DOTALL)
     if len(hit.groups()) >= 1:
         return hit.group(1)
@@ -146,18 +191,33 @@ def get_document_body(text) -> str:
 
 
 def remove_extra_newlines(doc_body: str):
-    """Remove extra newlines, i.e. more than 1 newlines in a row"""
+    """Remove extra newlines, i.e. more than 1 newlines in a row
+    Args:
+        text: The text of the latex document
+    Returns:
+        The text with extra newlines removed
+    """
     return re.sub(r"\n\n+", r"\n\n", doc_body)
 
 
 def get_abstract(text: str):
-    """Remove abstract enclosing if found"""
+    """Remove abstract enclosing if found
+    Args:
+        text: The text of the latex document
+    Returns:
+        The abstract and the text without the abstract
+    """
     abstract, text = get_text_enclosed(text, r"\\begin{abstract}", r"\\end{abstract}", block=True)
     return abstract, text
 
 
 def get_all_captions(text: str) -> list[str]:
-    """For all tables and figures, get the caption text and remove the table/figure env."""
+    """For all tables and figures, get the caption text and remove the table/figure env.
+    Args:
+        text: The text of the latex document
+    Returns:
+        A list with the text of all figure and table captions
+    """
     fig_tab_bodies = re.findall(
         r"\\begin{(?:figure|table)\*?}(.*?)\\end{(?:figure|table)\*?}", text, flags=re.DOTALL
     )
@@ -169,9 +229,13 @@ def get_all_captions(text: str) -> list[str]:
 
 
 def replace_subtitles(text: str):
-    """Replace the title, section or subsection titles with regular titles. Allowed are
+    r"""Replace the title, section or subsection titles with regular titles. Allowed are
     any number of subs before section and a \label{LABEL} in the section body.
     The LABEL is removed.
+    Args:
+        text: The text of the latex document
+    Returns:
+        The text with purely text-based titles (no latex commands)
     """
     # I don't join this function with get_title as this function operator on the body, only
     # and the title is outside the body
@@ -181,13 +245,24 @@ def replace_subtitles(text: str):
 
 
 def replace_refs(text):
-    """Replace all references by the number 1"""
+    """Replace all references by the number 1
+    Args:
+        text: The text of the latex document
+    Returns:
+        The body withouth any \ref{} commands
+    """
     return re.sub(r"\\ref{[^}]*}", "1", text)
 
 
 def replace_bib_with_caption(text: str, captions: list[str]):
     """We use the line including the bibliography to replace it with the captions
     of all figures and tables.
+    Args:
+        text: The text of the latex document
+        captions: The captions of all figures and tables
+    Returns:
+        The text with the bibliography replaced by the captions, which allows us to spellcheck
+        the captions.
     """
     # pylint: disable=anomalous-backslash-in-string
     captions_text = "\section{Captions}\n"
@@ -201,7 +276,7 @@ def replace_bib_with_caption(text: str, captions: list[str]):
 
 
 def main():
-    text_out = ""
+    """Format the text in a latex document to be spellchecked by removing latex commands"""
     mode = sys.argv[1]
     if mode == "detex":
         file = Path(sys.argv[2])
